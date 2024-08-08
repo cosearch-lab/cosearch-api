55d89f6dadbbe7c897632ed95f034fdc0e8bea43
Sébastien Ohleyer <sebastien.ohleyer@gmail.com>
2024-08-08 11:22:02 +0200

    Add Sentry for error reporting

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import sentry_sdk
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from structlog import get_logger
@@ -11,6 +12,14 @@ _LOGGER = get_logger()
 # from app.initial_data import init, init_data
 # init()
 # init_data()
+
+if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
+    sentry_sdk.init(
+        dsn=str(settings.SENTRY_DSN),
+        environment=settings.ENVIRONMENT,
+        enable_tracing=True,
+        traces_sample_rate=1.0,
+    )
 
 
 _LOGGER.info("Creating FastAPI app", settings=settings)
@@ -30,3 +39,9 @@ app.include_router(contributors.router, tags=["Contributors"])
 app.include_router(contributions.router, tags=["Contributions"])
 app.include_router(tags.router, tags=["Tags"])
 app.include_router(reviews.router, tags=["Reviews"])
+
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
+    return division_by_zero
